74bde02fbfc6fc24c25d5fbcd04da6225c762a37
Andrew Kelley <superjoe30@gmail.com>
2013-07-13 16:31:16 -0400

    perlin noise to generate population density

--- src/main.js
+++ src/main.js
@@ -1,18 +1,21 @@
 var chem = require("chem");
 var Vec2d = chem.vec2d;
+var perlin = require('./perlin');
 
 chem.onReady(function () {
   var canvas = document.getElementById("game");
   var engine = new chem.Engine(canvas);
   var imageData = engine.context.getImageData(0, 0, engine.size.x, engine.size.y);
   var cells = initializeCells();
+  renderAllCells();
   var brushRadius = 6;
   engine.on('update', function (dt, dx) {
     if (engine.buttonState(chem.button.MouseLeft)) {
       rasterCircle(engine.mousePos.x, engine.mousePos.y, brushRadius, function(x, y) {
         var cell = cellAt(x, y);
+        if (cell == null) return;
         cell.population += dx * 0.1;
-        renderCell(cell, x, y);
+        renderCell(y * engine.size.x + x);
       });
     }
   });
@@ -31,7 +34,7 @@ chem.onReady(function () {
     context.stroke();
 
     // draw a little fps counter in the corner
-    context.fillStyle = '#ffffff'
+    context.fillStyle = '#000000'
     engine.drawFps();
   });
   engine.start();
@@ -39,22 +42,33 @@ chem.onReady(function () {
   canvas.style.cursor = "none";
 
   function initializeCells() {
+    var noise = perlin.generatePerlinNoise(engine.size.x, engine.size.y, {
+      octaveCount: 6,
+      amplitude: 0.2,
+      persistence: 0.24,
+    });
     var cells = new Array(engine.size.x * engine.size.y);
-    var x = 0, y = 0;
     for (var i = 0; i < cells.length; ++i) {
       cells[i] = new Cell();
-      renderCell(cells[i], x, y);
-      x += 1;
-      if (x >= engine.size.x) {
-        x = 0;
-        y += 1;
+      var n = noise[i];
+      if (n > 0.50) {
+        cells[i].population = (n - 0.50) / 0.50;
+      } else {
+        cells[i].population = 0;
       }
     }
     return cells;
   }
 
-  function renderCell(cell, x, y) {
-    var index = (imageData.width * y + x) * 4;
+  function renderAllCells() {
+    for (var i = 0; i < cells.length; ++i) {
+      renderCell(i);
+    }
+  }
+
+  function renderCell(i) {
+    var cell = cells[i];
+    var index = i * 4;
     var value = 255 - (cell.population * 255);
     imageData.data[index + 0] = value; // red
     imageData.data[index + 1] = value; // green
@@ -97,10 +111,13 @@ function rasterCircle(x0, y0, radius, cb) {
     f += ddF_x;
     cb(x0 + x, y0 + y);
     cb(x0 - x, y0 + y);
+
     cb(x0 + x, y0 - y);
     cb(x0 - x, y0 - y);
+
     cb(x0 + y, y0 + x);
     cb(x0 - y, y0 + x);
+
     cb(x0 + y, y0 - x);
     cb(x0 - y, y0 - x);
   }
